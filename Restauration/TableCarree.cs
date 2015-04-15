9640f876ec86f848c6fa3c11601875b18b46fd00
paulo33m <paulo.m33@hotmail.fr>
2015-04-15 10:42:44 +0200

    Ajout des tables

---
+++ Restauration/TableCarree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    class TableCarree : Table
+    {
+        public TableCarree(int nbPlacesMin, int nbPlacesMax, string jumelage) : base(nbPlacesMin, nbPlacesMax, jumelage)
+        {
+        }
+    }
+}
---
+++ Restauration/TableRonde.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    class TableRonde : Table
+    {
+        public TableRonde(int nbPlacesMin, int nbPlacesMax, string jumelage) : base(nbPlacesMin,nbPlacesMax, jumelage)
+        {
+        }
+    }
+}
