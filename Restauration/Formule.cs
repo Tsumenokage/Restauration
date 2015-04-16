7161c7dbda6cae89e83b1b4e93a0a47a8419550a
paulo33m <paulo.m33@hotmail.fr>
2015-04-16 16:28:18 +0200

    Modif table avec {get;set;}

---
+++ Restauration/Formule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    abstract class Formule
+    {
+        protected string _type {get; set;}
+        protected double _dureePresence { get; set; }
+        protected double _dureePreparation { get; set; }
+        protected double _prix { get; set; }
+        public override string ToString()
+        {
+            string ch = "";
+            ch += "Type:" + _type + "     Présence:" + _dureePresence + "     temps de préparation:" + _dureePreparation + "     Prix:" + _prix;
+            return ch;
+        }
+    }
+}
--- Restauration/Table.cs
+++ Restauration/Table.cs
@@ -10,10 +10,10 @@ namespace Restauration
     {
         //champs nécessaires à la création d'une classe table
         protected static int _numTableTotal = 0; //numéro total de table dans le restaurant
-        protected int _numTable; //numéro de la table
-        protected int _nbPlacesMin; //nombre de place minimum de la table
-        protected int _nbPlacesMax; //nombre de place maximum de la table
-        protected string _jumelage; //O ou N en fonction de si la table peut être jumelée ou non
+        protected int _numTable { get; set; } //numéro de la table
+        protected int _nbPlacesMin { get; set; } //nombre de place minimum de la table
+        protected int _nbPlacesMax { get; set; } //nombre de place maximum de la table
+        protected string _jumelage { get; set; } //O ou N en fonction de si la table peut être jumelée ou non
         public Table(int nbPlaceMin, int nbPlaceMax, string jumelage)//contructeur de la classe table
         {
             //le numéro de la table est automatiquement attribué
@@ -23,28 +23,11 @@ namespace Restauration
             _nbPlacesMax = nbPlaceMax;
             _jumelage = jumelage;
         }
-        public int getnbPlacesMin()// permet de récupérer le nombre de place minimum de la table
-        {
-            return _nbPlacesMin;
-        }
-        public int getnbPlacesMax()// permet de récupérer le nombre de place maximum de la table
-        {
-            return _nbPlacesMax;
-        }
-        public int getnumTable() // permet de récupérer le numéro de la table
-        {
-            return _numTable;
-        }
-        public string getJumelage() // permet de savoir si la table est jumelable
-        {
-            return _jumelage;
-        }
         public override string ToString()
         {
             string ch = "";
             ch += "Numero:" + _numTable + "     nbPlaceMax:" + _nbPlacesMax + "     nbPlaceMin:" + _nbPlacesMin + "     Jumelage" + _jumelage;
             return ch;
         }
-
     }
 }
