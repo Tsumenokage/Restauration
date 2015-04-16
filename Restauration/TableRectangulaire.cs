27c8542a23ff4799b8e640859cdd7099e0b07aae
paulo33m <paulo.m33@hotmail.fr>
2015-04-16 15:21:45 +0200

    Re-ajout des tables
    Mauvaise manip...

---
+++ Restauration/TableRectangulaire.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    class TableRectangulaire : Table
+    {
+        public TableRectangulaire(int nbPlacesMin, int nbPlacesMax, string jumelage) : base(nbPlacesMin, nbPlacesMax, jumelage)
+        {
+        }
+    }
+}
