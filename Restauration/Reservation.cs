0aeb510be74d0ff109ab95d362bde79cc03a4453
Tsumenokage <Jeremy.tsume@Gmail.com>
2015-04-17 09:42:55 +0200

    Ajout classe Restauration.cs et Reservation.cs

---
+++ Restauration/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    class Reservation
+    {
+        private int _numeroReservation { get; set; } 
+        private String _nomClient;
+        private String _numeroTelephone;
+        private DateTime _dateReservation;
+        private int _nbConvives;
+        private Formule formuleRetenue;
+
+    }
+}
---
+++ Restauration/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Restauration
+{
+    class Restaurant
+    {
+        private String _nomRestaurant { get; set; }
+        private List<Table> _listeTables { get; set; }
+        private List<Formule> _listeFormules { get; set; }
+        
+
+    }
+}
