eaecfa95c595c8fec50b8cdc7f5956a388952429
Tsumenokage <Jeremy.tsume@Gmail.com>
2015-05-09 03:02:56 +0200

    ,ng
    hjhk

--- Restauration/Reservation.cs
+++ Restauration/Reservation.cs
@@ -27,6 +27,7 @@ namespace Restauration
             this._nbConvives = _nbConvives;
             this._formuleRetenue = _formuleRetenue;
             this._listeTablesReserve = new List<Table>();
+            
         }
 
         public override string ToString()
@@ -47,163 +48,239 @@ namespace Restauration
             return res;
         }
 
-        public List<Table> GestionReservation (string nomFormule, int nbConvives, DateTime dateReservation, Restaurant R)
+        public bool GestionReservation (string nomFormule, int nbConvives, DateTime dateReservation, Restaurant R)
         {
             Console.WriteLine("gestionreservation");
-            List<Table> tablesSelection = null;
-            int ressourceNecessairePreparation = 0;
+            List<Table> tablesLibre;
+
+
+            //On vérifie qu'il y a bien assez de ressources en cuisine
+            if (!verificationCuisine(nomFormule, nbConvives, dateReservation, R))
+            {
+                Console.WriteLine("Les ressources en cuisine ne sont pas suffisantes pour assurer cette réservation");
+                Console.ReadLine();
+                return false;
+            }
+
+            tablesLibre = rechercheTableLibre(nbConvives, R, dateReservation,nomFormule);
+            Console.WriteLine("Nombre de table libre " + tablesLibre.Count);
+
+            foreach (Table table in tablesLibre)
+            {
+                Console.WriteLine(table.ToString()); 
+            }
+            Console.ReadLine();
+
+
+            if(tablesLibre.Count == 0)
+            {
+                Console.WriteLine("Aucun tables n'est disponible pour cette horaire");
+                Console.ReadLine();
+                return false;
+            }
+
+            this._listeTablesReserve = rechercheTableReservation(nbConvives, tablesLibre);
+
+            return true;
+        }
+
+        //Cette fonction va vérifier si le personnel peut prendre en compte la commande
+        public bool verificationCuisine(string nomFormule, int nbConvives, DateTime dateReservation, Restaurant R)
+        {
             int ressourcePreparation = 0;
             int ressourceSalarie = 0;
-            bool ressourceValidation = true;
-            bool tableValidation = true;
-            int compteur = 0;
+            int ressourceNecessairePreparation = 0;
             DateTime finPreparation = new DateTime();
-            while (ressourceValidation != false && tableValidation != false && compteur ==0)
+
+            foreach (Salarie S in R._listeSalaries)
             {
-                foreach (Salarie S in R._listeSalaries)
-                {
-                    ressourceSalarie += S._ressource;
-                }
-                Formule form;
-                foreach (Formule Fo in R._listeFormules)
-                {
-                    form = R._listeFormules.Find(x => x._nomFormule == nomFormule);
-                    ressourceNecessairePreparation = ressourceNecessairePreparation + form._ressource * nbConvives;
-                }
-                Console.WriteLine(ressourceNecessairePreparation);
-                foreach (Reservation Reserve in R._listeReservations)
-                {
-                    form = R._listeFormules.Find(x => x._nomFormule == this._formuleRetenue);
-                    finPreparation = Reserve._dateReservation;
-                    finPreparation.AddHours(form._dureePreparation.Hour);
-                    finPreparation.AddMinutes(form._dureePreparation.Minute);
-                    if (dateReservation < finPreparation && dateReservation > Reserve._dateReservation)
-                    {
-                        ressourcePreparation += form._ressource * Reserve._nbConvives;
-                    }
-                }
-                Console.WriteLine(ressourcePreparation);
-                if (ressourceNecessairePreparation > (ressourceSalarie - ressourcePreparation))
-                {
-                    ressourceValidation = false;
-                }
-                Console.WriteLine(ressourceNecessairePreparation);
-                List<Table> tablesRestaurant = R._listeTables;
-                foreach (Reservation Rest in R._listeReservations)
-                {
-                    foreach (Table T in Rest._listeTablesReserve)
-                    {
-                        if (tablesRestaurant.Contains(T))
-                        {
-                            tablesRestaurant.Remove(T);
-                        }
-                    }
-                }
-                foreach (Table T in tablesRestaurant)
-                {
-                    Console.WriteLine(T.ToString());
-                }
-                tablesSelection = chercheTable(tablesRestaurant, nbConvives, R);
-                Console.WriteLine("if");
-                if (tablesSelection.Count == 0)
-                {
-                    Console.WriteLine("test");
-                    tableValidation = false;
-                }
-                else
-                {
-                    Console.WriteLine("test");
-                    foreach (Table t in tablesSelection)
-                    {
-                        t.ToString();
-                    }
-                    if (tablesSelection.Count > 0)
-                    {
-                        tableValidation = true;
-                    }
-                }
-                foreach (Table t in tablesSelection)
-                {
-                    _listeTablesReserve.Add(t);
-                }
-                compteur += 1;
+                ressourceSalarie += S._ressource;
             }
-            return tablesSelection;
+            Console.WriteLine("Ressource totale du restaurant : " + ressourceSalarie);
+
+
+            Formule form;
+
+            foreach (Formule Fo in R._listeFormules)
+            {
+                form = R._listeFormules.Find(x => x._nomFormule == nomFormule);
+                ressourceNecessairePreparation = ressourceNecessairePreparation + form._ressource * nbConvives;
+            }
+
+            Console.WriteLine(ressourceNecessairePreparation);
+            Console.ReadLine();
+
+            foreach (Reservation Reserve in R._listeReservations)
+            {
+                form = R._listeFormules.Find(x => x._nomFormule == this._formuleRetenue);
+                finPreparation = Reserve._dateReservation;
+                finPreparation.AddHours(form._dureePreparation.Hour);
+                finPreparation.AddMinutes(form._dureePreparation.Minute);
+                if (dateReservation < finPreparation && dateReservation > Reserve._dateReservation)
+                {
+                    ressourcePreparation += form._ressource * Reserve._nbConvives;
+                }
+            }
+
+            Console.WriteLine(ressourcePreparation);
+
+            if (ressourceNecessairePreparation > (ressourceSalarie - ressourcePreparation))
+            {
+                return false;
+            }
+            else
+                return true;
         }
-        public List<Table> chercheTable(List<Table> tablesRestaurant, int nbConvives, Restaurant R)
+
+        //Cette fonction va retourner la listes des tables libre à la période de la reservation
+        public List<Table> rechercheTableLibre(int nbConvives, Restaurant R, DateTime dateReservation, String nomFormule)
         {
-            Console.WriteLine("test5555");
-            List<Table> tables = new List<Table>();
-            Table tableNonJumelee = null;
-            Table tableStockée = null;
-            bool tableTrouve = false;
-            while (tableTrouve == false)
+            List<Table> tableReserves = new List<Table>();
+            List<Table> tableLibre = new List<Table>();
+            DateTime finReservationEnCoursEnregistrement = dateReservation;
+            Formule formuleReservationEnCoursEnregistrement = R._listeFormules.Find(x => x._nomFormule == nomFormule);
+            finReservationEnCoursEnregistrement =  finReservationEnCoursEnregistrement.AddHours(formuleReservationEnCoursEnregistrement._dureePresence.Hour);
+            finReservationEnCoursEnregistrement = finReservationEnCoursEnregistrement.AddMinutes(formuleReservationEnCoursEnregistrement._dureePresence.Minute);
+
+
+            //Dans un premier temps on récupère les tables non utilisées
+            foreach (Reservation reserv in R._listeReservations)
             {
-                Console.WriteLine("test");
-                tableNonJumelee = tablesRestaurant.Find(x => x._nbPlacesMax == nbConvives && x._jumelage == false);
-                if (tableNonJumelee != null)
+                //On calcule la date de Fin de chaque reservation
+                DateTime finPresenceClientDejaReserve = reserv._dateReservation;
+                Formule formuleReservation = R._listeFormules.Find( x => x._nomFormule == reserv._formuleRetenue);
+                finPresenceClientDejaReserve = reserv._dateReservation.AddHours(formuleReservation._dureePresence.Hour);
+                finPresenceClientDejaReserve = reserv._dateReservation.AddMinutes(+formuleReservation._dureePresence.Minute);
+
+                //Si le début de la réservation en cours d'enregistrement 
+                //empiète sur une autre reservation, on enregistre les tables déjà reservé
+                if (dateReservation <= finPresenceClientDejaReserve)
                 {
-                    tables.Add(tableNonJumelee); 
-                    tableTrouve = true;
-                }
-                else
-                {
-                    foreach (Table T2 in tablesRestaurant)
+                    foreach (Table table in reserv._listeTablesReserve)
                     {
-                        if (T2._nbPlacesMax > nbConvives & !T2._jumelage)
+                        if(table.GetType().ToString() != "Restauration.TableRonde" )
+                            tableReserves.Add(table);
+                        else
                         {
-                            if (tables.Count == 0)
-                            { 
-                                tableStockée = T2;
-                                tables.Add(tableStockée); 
-                            }
-                            if (tableStockée._nbPlacesMax > T2._nbPlacesMax)
-                            {
-                                tables.Remove(tableStockée);
-                                tableStockée = T2;
-                                tables.Add(tableStockée);
-                            }
-                        }
-                    }
-                    Console.WriteLine("...");
-                    if (tables.Count != 0)
-                    {
-                        tableTrouve = true;
-                    }
-                    foreach (Table T3 in tablesRestaurant)
-                    {
-                        if (T3._jumelage)
-                        {
-                            foreach (Table T4 in tablesRestaurant)
-                            {
-                                //jumelage table rectangulaire 4 places
-                                if (T4._jumelage & (T3._numTable != T4._numTable) & (T3._nbPlacesMax == 4) & (T3._nbPlacesMax == T4._nbPlacesMax) & ((T3._nbPlacesMax+T4._nbPlacesMax)==nbConvives) & (T3 is TableRectangulaire) & (T4 is TableRectangulaire))
-                                {
-                                    tables.Add(T3);
-                                    tables.Add(T4);
-                                    tableTrouve = true;
-                                }
-                                //jumelage table rectangulaire  > 4 places
-                                else if (T4._jumelage & (T3._nbPlacesMax > 4) & (T3._nbPlacesMax == T4._nbPlacesMax) & ((T3._nbPlacesMax + T4._nbPlacesMax)-2 == nbConvives) & (T3 is TableRectangulaire) & (T4 is TableRectangulaire))
-                                {
-                                    tables.Add(T3);
-                                    tables.Add(T4);
-                                    tableTrouve = true;
-                                }
-                                else if (T4._jumelage & (T3._nbPlacesMax == T4._nbPlacesMax) & (((T3._nbPlacesMax+T4._nbPlacesMax) - ((T3._nbPlacesMax/4)*2))==nbConvives) & (T3 is TableCarree) & (T4 is TableCarree))
-                                {
-                                    tables.Add(T3);
-                                    tables.Add(T4);
-                                    tableTrouve = true;
-                                }
-                            }
+                            if (table._nbPlacesMax >= nbConvives)
+                                tableReserves.Add(table);
                         }
                     }
                 }
-                Console.WriteLine("euh");
-                Console.ReadLine();
+                
+                //Si la fin de la reservation empiète sur une autre reservation
+                if (finReservationEnCoursEnregistrement > reserv._dateReservation)
+                {
+                    foreach (Table table in reserv._listeTablesReserve)
+                    {
+                        if (table.GetType().ToString() != "Restauration.TableRonde")
+                            tableReserves.Add(table);
+                        else
+                        {
+                            if (table._nbPlacesMax >= nbConvives)
+                                tableReserves.Add(table);
+                        }
+                    }
+                }
             }
-            return tables;
+
+            //On crée ensuite la liste des tables libre
+            foreach (Table table in R._listeTables)
+            {
+                //Si la table courante n'est pas dans la listes des tables reservés
+                if(!tableReserves.Contains(table))
+                {
+                    //On ajoute la table dans la liste des tables libre
+                    tableLibre.Add(table);
+                }
+            }
+            return tableLibre;
+
+        }
+
+        public List<Table> rechercheTableReservation(int nbConvive, List<Table> tablesLibre)
+        {
+            List<Table> tablesReservations = new List<Table>();
+            bool numOk = false;
+            int numeroTable = -1;
+            int nbPlaceCarreeTotale = 0;
+            int nbPlaceRectangulaireTotale = 0;
+
+            foreach (Table table in tablesLibre)
+            {
+                if (table.GetType().ToString() == "Restauration.TableCarree")
+                    nbPlaceCarreeTotale += table._nbPlacesMax;
+            }
+
+            if (nbPlaceCarreeTotale < nbConvive)
+            {
+                tablesReservations.RemoveAll(x => x.GetType().ToString() == "Restauration.TableCarree");
+            }
+
+
+            foreach (Table table in tablesLibre)
+            {
+                if (table.GetType().ToString() == "Restauration.TableRectangulaire")
+                    nbPlaceRectangulaireTotale += table._nbPlacesMax;
+            }
+
+            if (nbPlaceRectangulaireTotale < nbConvive)
+            {
+                tablesReservations.RemoveAll(x => x.GetType().ToString() == "Restauration.TableRectangulaire");
+            }
+
+            if (tablesLibre.Count == 0)
+                return tablesReservations;
+
+            Console.WriteLine("Voici la listes des tables disponibles : ");
+            foreach (Table table in tablesLibre)
+            {
+                Console.WriteLine(table.ToString()); 
+            }
+
+            
+            while (!numOk)
+            {
+                Console.WriteLine("Entrez le numéro de la table que vous souhaitez utiliser");
+                numOk = int.TryParse(Console.ReadLine(), out numeroTable);
+                tablesReservations.Add(tablesLibre.Find(x => x._numTable == numeroTable));
+                if(tablesReservations.Count == 0)
+                    numOk = false;
+            }
+
+
+
+            //On retire des tables libres celle ne disposant pas du même types que la première table choisis
+            tablesLibre.RemoveAll(x => x.GetType() != tablesReservations[0].GetType());
+            //On retire des tables libres la tables que l'on vient de choisir
+            tablesLibre.RemoveAll(x => x._numTable == numeroTable);
+
+            while (tablesReservations.Sum(x => x._nbPlacesMax) < nbConvive)
+            {
+                numOk = false;
+                Console.WriteLine("Nombre de place insuffisant, veuillez selectionner une table à jumeler");
+                foreach (Table table in tablesLibre)
+                {
+                    if(table.GetType() == tablesReservations[0].GetType())
+                    {
+                        Console.WriteLine(table.ToString()); 
+                    }
+                }
+
+                int oldCount = tablesReservations.Count;
+                while (!numOk)
+                {
+                    int numeroTableJumelable;
+                    Console.WriteLine("Entrez le numéro de la table que vous souhaitez utiliser");
+                    numOk = int.TryParse(Console.ReadLine(), out numeroTableJumelable);
+                    tablesReservations.Add(tablesLibre.Find(x => x._numTable == numeroTableJumelable));
+                    if (tablesReservations.Count == oldCount)
+                        numOk = false;
+                }
+                tablesLibre.RemoveAll(x => x._numTable == numeroTable);
+            }
+
+            return tablesReservations;
         }
     }
 }
